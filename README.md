321e15248039a1d35e6b7d9a0d4836a4c26a28a9
unspeakable2008 <64965483+unspeakable2008@users.noreply.github.com>
2020-06-18 16:05:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tower-seige-1_
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,46 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	ground = new Ground(600,height,800,20);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	//World.add(world, packageBody);
+	
+
+	//Create a Ground
+	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	//World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ground.display();	
+
+  drawSprites();
+ 
+}
+
+
+
